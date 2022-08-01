9afb2f11a0b3ff64636372036ea504867381b163
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2022-07-31 23:35:37 -0400

    chore: update docker-compose for server and web
    * chore(docs): simplify instructions
    
    * chore(scripts): create env file with random values in server
    
    * chore(server): clairBuoyant/server#40
    
    * chore(web): clairBuoyant/web#18

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,34 @@
-#!/bin/sh
+#!/bin/sh -e
 
-# Exit in case of error
-set -e
+random_password () {
+    date +%s | sha256sum | base64 | head -c 32 ; echo
+}
+set_username () {
+    whoami
+}
 
-# CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-# BASE_DIR="$(dirname "$CURRENT_DIR")"
+# TODO: guard for GitHub Actions
+# how to handle setting .env in github actions and sharing creds
+if [ ! -f server/.env ]
+then
+    touch server/.env
+    echo "POSTGRES_DB=devbuoyant" >> server/.env
+    echo "POSTGRES_PASSWORD=$(random_password)" >> server/.env
+    echo "POSTGRES_USER=$(set_username)" >> server/.env
+    echo "DATABASE_URL=postgresql+asyncpg://$(set_username):$(random_password)@localhost:5432/clairbuoyant" >> server/.env
+    echo "PYTHON_ENV=development" >> server/.env
+fi
 
-# cd $BASE_DIR
+# LOAD ENV VARIABLES
+if [ -f server/.env ]; then
+    export $(echo $(cat server/.env | sed 's/#.*//g'| xargs) | envsubst)
+fi
 
-# docker build --tag clairbuoyant-server --file docker/Dockerfile .
+# Install node_modules before caching in Docker
+cd web && npm ci && cd -
 
 # Build and run containers
-docker-compose up -d
+COMPOSE_PROJECT_NAME=devbuoyant docker-compose up -d
 
 # Hack to wait for postgres container to be up before running alembic migrations
 sleep 10;
@@ -22,5 +39,5 @@ docker-compose run --rm backend alembic upgrade head
 # Create initial data
 docker-compose run --rm backend python3 server/seed_initial_data.py
 
-# Uncomment to load geodata (WIP)
-# docker-compose run --rm / ./db_scripts/load_geodata.sh
+# Uncomment to load geodata
+# docker-compose run --rm postgres ./db_scripts/load_geodata.sh
